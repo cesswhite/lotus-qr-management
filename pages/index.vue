f9c7815debde9d8975ae555667e22bff407dc052
Cess White <acessloop@gmail.com>
2024-10-24 13:08:35 -0600

    [ADD] - Minor fixes in UI and UX v3

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <section id="id_section-hero">
-        <header class="flex items-center justify-end px-4 fixed top-0 left-0 w-full">
+        <header class="flex items-center justify-end px-4 fixed top-4 right-0 w-full">
             <AppSwitchMode />
         </header>
         <IndexHero />
